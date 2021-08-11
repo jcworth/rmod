7c8435f888dddd5a2c0c11815a47ec69cb599ad2
Jack Holdsworth <jack.holdsworth@fxdigital.uk>
2021-08-11 14:56:03 +0100

    refactor: refactor recursive fns into struct

--- src/recursive.rs
+++ src/recursive.rs
@@ -1,49 +1,113 @@
-use std::{fs, os::macos::fs::MetadataExt, path::Path};
+use std::{
+    fs::{self, DirEntry, ReadDir},
+    os::macos::fs::MetadataExt,
+    path::{Path, PathBuf},
+};
 
 use rug::Float;
 
 use crate::{error::RmError, utils, NodeModuleMap};
 
-pub fn recursive_search(dir: &Path, module_map: &mut NodeModuleMap) -> Result<(), RmError> {
-    let path = fs::read_dir(dir)?
-        .filter_map(Result::ok)
-        .filter(|e| !utils::is_hidden(e));
-
-    for entry in path {
-        let file_path_buf = entry.path();
-        if let Ok(attribs) = file_path_buf.metadata() {
-            let file_type = &attribs.file_type();
-
-            if file_type.is_symlink() {
-                continue;
-            } else if file_type.is_dir() && utils::is_node_modules(&file_path_buf) {
-                module_map.add(file_path_buf);
-            } else if file_type.is_dir() {
-                recursive_search(&file_path_buf, module_map)?;
-            }
-        }
-    }
-    Ok(())
+#[derive(Debug)]
+pub struct Recursive {
+    dir: ReadDir,
+    store: NodeModuleMap
 }
 
-pub fn recursive_count(dir: &Path) -> Result<Float, RmError> {
-    // @TODO: make block calc platform generic - currently unix/macos
-    let path = fs::read_dir(dir)?.filter_map(Result::ok);
-    let mut total_size = Float::with_val(32, 0.0);
+impl Recursive {
+    pub fn new(path: &str) -> Result<Self, RmError> {
+        let p = fs::read_dir(path)?;
 
-    for entry in path {
-        let file_path_buf = entry.path();
-        if let Ok(attribs) = file_path_buf.metadata() {
-            let file_type = &attribs.file_type();
+        Ok(Self { dir: p, store: NodeModuleMap::new() })
+    }
 
-            if file_type.is_symlink() {
-                continue;
-            } else if file_type.is_dir() {
-                total_size += recursive_count(&file_path_buf)?;
-            } else {
-                total_size += Float::with_val(32, attribs.st_blocks() * 512);
+    pub fn search(&self) -> Result<(), RmError> {
+        let entries = self
+            .dir
+            .filter_map(Result::ok)
+            .filter(|e| !utils::is_hidden(e));
+
+        for entry in entries {
+            // println!("{:?}", entry)
+            let file_path_buf = entry.path();
+            if let Ok(attribs) = file_path_buf.metadata() {
+                let file_type = &attribs.file_type();
+
+                if file_type.is_symlink() {
+                    continue;
+                } else if file_type.is_dir() && utils::is_node_modules(&file_path_buf) {
+                    self.store.add(file_path_buf);
+                } else if file_type.is_dir() {
+                    Self::search(self)?;
+                }
             }
         }
+        Ok(())
     }
-    Ok(total_size)
+
+    pub fn count(&self) -> Result<Float, RmError> {
+        // @TODO: make block calc platform generic - currently unix/macos
+        let entries = self.dir.filter_map(Result::ok);
+        let mut total_size = Float::with_val(32, 0.0);
+
+        for entry in entries {
+            let file_path_buf = entry.path();
+            if let Ok(attribs) = file_path_buf.metadata() {
+                let file_type = &attribs.file_type();
+
+                if file_type.is_symlink() {
+                    continue;
+                } else if file_type.is_dir() {
+                    total_size += Self::count(self)?;
+                } else {
+                    total_size += Float::with_val(32, attribs.st_blocks() * 512);
+                }
+            }
+        }
+        Ok(total_size)
+    }
 }
+
+// pub fn recursive_search(dir: &Path, module_map: &mut NodeModuleMap) -> Result<(), RmError> {
+//     let path = fs::read_dir(dir)?
+//         .filter_map(Result::ok)
+//         .filter(|e| !utils::is_hidden(e));
+
+//     for entry in path {
+//         let file_path_buf = entry.path();
+//         if let Ok(attribs) = file_path_buf.metadata() {
+//             let file_type = &attribs.file_type();
+
+//             if file_type.is_symlink() {
+//                 continue;
+//             } else if file_type.is_dir() && utils::is_node_modules(&file_path_buf) {
+//                 module_map.add(file_path_buf);
+//             } else if file_type.is_dir() {
+//                 recursive_search(&file_path_buf, module_map)?;
+//             }
+//         }
+//     }
+//     Ok(())
+// }
+
+// pub fn recursive_count(dir: &Path) -> Result<Float, RmError> {
+//     // @TODO: make block calc platform generic - currently unix/macos
+//     let path = fs::read_dir(dir)?.filter_map(Result::ok);
+//     let mut total_size = Float::with_val(32, 0.0);
+
+//     for entry in path {
+//         let file_path_buf = entry.path();
+//         if let Ok(attribs) = file_path_buf.metadata() {
+//             let file_type = &attribs.file_type();
+
+//             if file_type.is_symlink() {
+//                 continue;
+//             } else if file_type.is_dir() {
+//                 total_size += recursive_count(&file_path_buf)?;
+//             } else {
+//                 total_size += Float::with_val(32, attribs.st_blocks() * 512);
+//             }
+//         }
+//     }
+//     Ok(total_size)
+// }
--- src/search.rs
+++ src/search.rs
@@ -7,23 +7,24 @@ use std::{
     thread::{self, JoinHandle},
 };
 
-use crate::{error::RmError, recursive, Config, NodeModuleMap};
+use crate::{Config, NodeModuleMap, error::RmError, recursive::{self, Recursive}};
 
 pub fn init_search(
     is_searching: Arc<AtomicBool>,
     config: &Config,
-) -> JoinHandle<Result<NodeModuleMap, RmError>> {
+) -> Result<NodeModuleMap, RmError> {
     // Run search & count in separate thread
     let target_dir = config.target_dir.clone();
-    thread::spawn(move || -> Result<NodeModuleMap, RmError> {
+    let r = Recursive::new(&target_dir)?;
+    // thread::spawn(move || -> Result<NodeModuleMap, RmError> {
         let mut node_map = NodeModuleMap::new();
 
         // search and on ok count
-        match recursive::recursive_search(Path::new(&target_dir), &mut node_map) {
+        match r.search() {
             Ok(_) => {
                 // Store size of node folders
                 for dir in &mut node_map.dirs {
-                    let new_file_size = recursive::recursive_count(dir.0).unwrap();
+                    let new_file_size = r.count().unwrap();
                     // bytes to mb
                     *dir.1 += new_file_size / 1000 / 1000;
                 }
@@ -33,5 +34,5 @@ pub fn init_search(
             }
             Err(_) => return Err(RmError::Io),
         }
-    })
+    // })
 }
