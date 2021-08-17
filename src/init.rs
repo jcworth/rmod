fdf6c126e64f2c358e51e8df43f05a8dec4ed038
Jack Holdsworth <jack.holdsworth@fxdigital.uk>
2021-08-17 17:42:35 +0100

    refactor: finish recursive to walk rename

--- src/init.rs
+++ src/init.rs
@@ -6,30 +6,30 @@ pub fn run(config: Config) -> Result<NodeModuleMap, RmError> {
     // Check target_dir
     utils::is_directory_valid(&config.target_dir)?;
 
-    let r = Walk::new(&config.target_dir)?;
+    let walk = Walk::new(&config.target_dir)?;
     let mut nm_map = NodeModuleMap::new();
 
     // search and on ok count
-    let path = Path::new(&r.dir);
-    match r.search(path, &mut nm_map) {
+    let path = Path::new(&walk.dir);
+    match walk.search(path, &mut nm_map) {
         Ok(_) => {
             // Store size of node_module folders
             let mut all_dirs_size = 0.0;
-            r.spinner.set_count_style();
+            walk.spinner.set_count_style();
 
             for dir in &mut nm_map.dirs {
                 let mut dir_size = *dir.1;
-                dir_size += r.count(dir.0)?;
+                dir_size += walk.count(dir.0)?;
 
                 // Add individual dir_size to total
                 all_dirs_size += dir_size;
-                r.spinner
+                walk.spinner
                     .msg(format!("{:.2}", FileSize::MB.get_value(all_dirs_size)));
             }
 
             // bytes to mb on total val
             nm_map.total_size += FileSize::MB.get_value(all_dirs_size);
-            r.spinner.end();
+            walk.spinner.end();
 
             Ok(nm_map)
         }
